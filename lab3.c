47c1e71a2caf4cd4b849e85fa01bd2e82aec56c0
Daniel Evertsson <danne_master@yahoo.se>
2011-10-05 13:32:59 +0200

    Nu kan programmet läsa flera sökvägar från kommando raden

--- lab3.c
+++ lab3.c
@@ -6,112 +6,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-typedef struct cell{
-
-        struct cell * prev;
-        void * value;
-        struct cell * next;
-
-}cell;
-
-typedef struct {
-
-        cell * head;
-        cell * tail;
-
-}head_tail;
-
-head_tail create_list(void){
-
-        head_tail h_t;
-
-        h_t.head = (cell *)calloc(1, sizeof(cell));
-        h_t.head->next = (cell *)calloc(1, sizeof(cell));
-
-        h_t.tail = h_t.head->next;
-
-        h_t.tail->prev = h_t.head;
-
-        return h_t;
-
-}
-
-void insert(void * value, cell * pos, head_tail * h_t){
-
-        cell * ny_cell;
-
-        ny_cell = (cell *)calloc(1, sizeof(cell));
-
-        ny_cell->next = pos;
-
-        ny_cell->prev = pos->prev;
-
-        pos->prev->next = ny_cell;
-
-        pos->prev = ny_cell;
-
-        pos->prev->value = value;
-
-}
-
-cell * first(head_tail * h_t){
-
-        if (h_t->tail->prev != h_t->head){
-                return h_t->head->next;
-        }
-
-        return h_t->tail;
-}
-
-cell * remove_cell(cell * pos, head_tail * h_t){
-
-        cell * temp;
-
-        pos->next->prev = pos->prev;
-
-        pos->prev->next = pos->next;
-
-        temp = pos->next;
-
-        free (pos);
-
-        return temp;
-
-}
-
-void * inspect(cell * pos, head_tail * h_t){
-
-        return pos->value;
-
-}
-
-/*  lägger alla mappar den ska besöka i en lista
-    insert(dir->d_name, first(&h_t), &h_t);
-    str = inspect(first(&h_t), &h_t);
-    printf("%s\n", str);
-*/
 
  int search(char c, char * searchString, char * path){
 
     DIR * d;
     struct dirent *dir;
 	struct stat	buf;
-	char* newPath;
     char currentPath[4096];
-
-
-    head_tail h_t;
-    char * str;
-    h_t = create_list();
 
     if (chdir(path) < 0)
         printf("chdir failed\n");
-/*
-    //printf("-->%s\n", currentPath); /home/evert/etc/example/lab3/test/
 
-    if (chdir("test") < 0)
-        printf("chdir failed\n");
-*/
     if ((d = opendir(".")) == NULL){
         printf("Kan inte öppna sökvägen");
         return 0;
@@ -196,6 +101,7 @@ int getFileIndex(int argc, char **argv){
             return i;
         }
     }
+    return 0;
 }
 
 
@@ -217,78 +123,36 @@ int getPathStartIndex(int argc, char **argv){
 int main (int argc, char **argv) {
 
     int c;
-    int fileIndex;
     char * searchString;
     int flagExist = 0;
     char * path;
-    int pathIndex;
+    int i;
 
     opterr = 0;
     searchString = strdup(argv[getFileIndex(argc, argv)]);
 
-/*
-   char currentPath[4096];
+    for(i = 1; i < getFileIndex(argc, argv); i++){
 
-    if (getcwd(currentPath, sizeof(currentPath)) == NULL){
-        printf("fail to do getcwd!");
-    }
+        while ((c = getopt (argc, argv, "t:")) != -1){
 
+            path = argv[i - 1];
 
-    if (chdir("test") < 0)
-        printf("chdir failed\n");
-*/
+            if (c == 't'){
 
+                flagExist = 1;
+                search(optarg[0], searchString, path);
 
-    while ((c = getopt (argc, argv, "t:")) != -1){
-
-        pathIndex = getPathStartIndex(argc, argv);
-        path = argv[pathIndex];
-
-        if (c == 't'){
-
-            flagExist = 1;
-            search(optarg[0], searchString, path);
-/*
-            switch (optarg[0]){
-                    case 'd':
-                        search('d', searchString);
-                        break;
-
-                    case 'f':
-                        search('f', searchString);
-                        break;
-
-                    case 'l':
-                        search('l', searchString);
-                        break;
-
-                    default:
-                        printf("The file type doesn,t exist\n");
-                        break;
-                }
-*/
+            }
+            else{
+                printf("The flag doesn't exist!");
+            }
         }
-        else{
-            printf("The flag doesn't exist!");
+
+        if (!flagExist){
+            path = argv[i];
+            search('a', searchString, path);
         }
     }
 
-    if (!flagExist){
-        path = argv[1];
-        search('a', searchString, path);
-    }
-
-/*
-    for (index = optind; index < argc; index++){
-
-        if (index <= (argc -2)){
-            printf ("Paths: %s\n", argv[index]);
-        }
-
-        else if (index == (argc -1)){
-            printf ("File: %s\n", argv[index]);
-        }
-    }
-*/
     return 0;
 }
